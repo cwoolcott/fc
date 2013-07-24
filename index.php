4e2aa82e679af7747a8e1462b13c023a2417698e
cwoolcott <CWoolcott@unityworksmedia.com>
2013-07-24 14:43:52 -0500

    New Image-Based FlipClock

--- index.php
+++ index.php
@@ -1,32 +1,60 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"><head>
+<!DOCTYPE html>
 <html>
 <head>
-<!-- UW_CLOCK -->
-<link rel="stylesheet" href="flipclock/flipclock.css">
-<link rel="stylesheet" href="flipclock/uw_fc.css">
-
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-<script src="flipclock/prefixfree.min.js"></script>
-<script src="flipclock/flipclock.min.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>UW Counter Demo</title>
+<link rel="stylesheet" type="text/css" href="css/uw_fc.css">
+<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
 </head>
 <body>
-
-<div class="container">
+<div id="container" class="content clearfix">
     <div class="toprightcontainer">
-    	<div class="uw_text" ></div>
-        <div class="uw_clock_border">
-        <div id="uw_clock" ></div>
+        <div class="uw_text">TEST</div>
+    <div class="uw_clock_border">
+        <div id="counter">				
+            <div id="shading">&nbsp;</div>
         </div>
-    </div>
-</div>
-
+        <div id="numbers"></div>
+     </div>		
+    </div>   
+</div> 
+<script type="text/javascript" src="js/C3counter.js"></script>
 <script type="text/javascript">
-var enddate='07/29/2013'; //Event Ending
-var rightnow = <?php echo strtotime("now")*1000; ?>; //server-side init
+var countdown=0;
+
+// Default options 
+var enddate='07/28/2013'; //Event Ending
+var rightnow = '<?php echo strtotime("now")*1000; ?>'; //server-side init
+
+if (rightnow==""){rightnow=0};
+
+var d2 = new Date(enddate);
+var eventtime = d2.getTime(); 
+
+if (eventtime<rightnow) {
+	var countdown=0;
+}
+else
+{
+	var countdown=(eventtime-rightnow)/1000;
+}
+
+var countdowntext='Ends<br/>Soon!';
+
+if (countdown<1){
+	countdowntext='';
+}else if (countdown<86400){
+	countdowntext='Ends<br/>Today!';
+}else if (countdown<172800){
+	countdowntext='Ends<br/>Tomorrow!';
+}else if (countdown<604800){
+	countdowntext='Ends<br/>This Week!';
+}else if(countdown<1209600){
+	countdowntext='Ends<br/>in Two Weeks!';}
+	
+$('div.uw_text').html(countdowntext);
+
+C3Counter("counter", { startTime :countdown },rightnow);
 </script>
-
-<script src="flipclock/uw_fc.js"></script>
-
-<!-- END UW_CLOCK -->
-	</body>
+</body>
 </html>
